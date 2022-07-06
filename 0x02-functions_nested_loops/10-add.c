e7f8916140d82374f9e3069012d0338a6d45e4e6
njokimatheri <njokimatheri85@gmail.com>
2022-07-06 02:55:16 -0700

    unction that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers from input
+ * @x: parameter 1
+ * @y: parameter 2
+ *
+ * Description: adds two numbers
+ * Return: Always (0).
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+
+}
