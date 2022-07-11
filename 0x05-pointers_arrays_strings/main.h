e5875a92047a49c13b71cceffb67e97e52f8286b
njokimatheri <njokimatheri85@gmail.com>
2022-07-11 05:10:24 -0700

    unction that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,30 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+int _strleni(int *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+int _putchar(char c);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+
+
+#endif /* _HOLBERTON_H_ */
