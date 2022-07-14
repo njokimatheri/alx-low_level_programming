1e764f990bfea01c240e80491eb785d545a0faff
njokimatheri <njokimatheri85@gmail.com>
2022-07-14 02:20:53 -0700

    More pointers

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,31 @@
+#ifndef _HOLBERTON_H_
+
+#define _HOLBERTON_H_
+
+
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+int _putchar(char c);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *a);
+
+char *cap_string(char *a);
+
+char *leet(char *a);
+
+char *rot13(char *);
+
+void print_number(int n);
+
+
+
+#endif /* _HOLBERTON_H_ */
